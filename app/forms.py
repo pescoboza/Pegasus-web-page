6ba5ad7d4fd804760b96526dab5e93f87871a4e1
lolo23jhon <lolo23jhon@gmail.com>
2020-03-19 21:38:29 -0700

    Added dictionary for field lengths: forms.FIELD_LENGTHS. It is shared between forms and models.

--- app/forms.py
+++ app/forms.py
@@ -4,29 +4,59 @@ from wtforms import TextField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Length, Email
 import copy
 
-fields = {
-    "username":
-     lambda required=True: TextField(label="Username",  
-                validators=[Length(min=8, max=16), InputRequired() if required else None]),
-    "password": 
-    lambda required=True: PasswordField(label="Password",
-                validators=[Length(min=8, max=60), InputRequired() if required else None]),
-    "email":
-    lambda required=True: TextField(label="Email",
-                validators=[InputRequired() if required else None]),
+
+FIELD_LENGTHS = {
+    "username": {
+        "min": 8,
+        "max": 16,
+        "message": None
+    },
+    "password": {
+        "min": 8,
+        "max": 30,
+        "message": None
+    },
+    "firstName": {
+        "min": 1,
+        "max": 30,
+        "message": None
+    },
+    "lastName": {
+        "min": 1,
+        "max": 30,
+        "message": None
+    },
+    "email": {
+        "min": 5,
+        "max": 254,
+        "message": None
+    },
 }
 
+flen = FIELD_LENGTHS
 
 class LoginForm(FlaskForm):
-    username = fields["username"]()
-
-    password = fields["password"]()
+    username = TextField(label="Username", validators=[
+                         InputRequired(), Length(**flen["username"])])
+    password = PasswordField(label="Password", validators=[
+                             InputRequired(), Length(**flen["password"])])
     remember = BooleanField()
 
 
 class RegisterForm(FlaskForm):
-    username = fields["username"]()
-    
-    password = fields["password"]()
-    enter_password_again = fields["password"]()
+    firstName = TextField(label="First Name", validators=[
+                          InputRequired(), Length(**flen["firstName"])])
+    lastName = TextField(label="Last Name", validators=[
+                         InputRequired(), Length(**flen["lastName"])])
+    username = TextField(label="Username", validators=[
+                         InputRequired(), Length(**flen["username"])])
+    email = TextField(label="Email", validators=[
+                      InputRequired(), Length(**flen["email"])])
 
+    password_first = PasswordField(label="Password", validators=[
+                                   InputRequired(), Length(**flen["password"])])
+    password_again = PasswordField(label="Password", validators=[
+                                   InputRequired(), Length(**flen["password"])])
+
+    newsletter = BooleanField()
+    accept_terms_and_conditions = BooleanField()
