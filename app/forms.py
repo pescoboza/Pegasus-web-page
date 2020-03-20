6e3c9390f409ab877c5b3be01a2130016f57d8f6
lolo23jhon <lolo23jhon@gmail.com>
2020-03-19 20:22:12 -0700

    Added modular fields for forms in forms.py.

--- app/forms.py
+++ app/forms.py
@@ -1,13 +1,32 @@
 from flask_wtf import FlaskForm
 import wtforms
 from wtforms import TextField, PasswordField, BooleanField
-from wtforms.validators import InputRequired, Length
+from wtforms.validators import InputRequired, Length, Email
+import copy
+
+fields = {
+    "username":
+     lambda required=True: TextField(label="Username",  
+                validators=[Length(min=8, max=16), InputRequired() if required else None]),
+    "password": 
+    lambda required=True: PasswordField(label="Password",
+                validators=[Length(min=8, max=60), InputRequired() if required else None]),
+    "email":
+    lambda required=True: TextField(label="Email",
+                validators=[InputRequired() if required else None]),
+}
 
 
 class LoginForm(FlaskForm):
-    username = TextField(label="Username", 
-                        validators=[InputRequired(),Length(min=8, max=16)])
+    username = fields["username"]()
 
-    password = PasswordField(label="Password",
-                             validators=[InputRequired(), Length(min=8, max=60)], description="feferef")
+    password = fields["password"]()
     remember = BooleanField()
+
+
+class RegisterForm(FlaskForm):
+    username = fields["username"]()
+    
+    password = fields["password"]()
+    enter_password_again = fields["password"]()
+
