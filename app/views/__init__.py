994a90f203a593d0b1526087c1a264ee293ac19a
lolo23jhon <lolo23jhon@gmail.com>
2020-03-21 16:07:23 -0700

    The login form now correctly queries the database to validate user and password.

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, redirect, request
 from ..models.user import User
 from .. import app, db
 from ..forms.login_form import LoginForm
@@ -19,24 +19,25 @@ def index():
 def login():
 
     form = LoginForm()
+    message = None
 
     # Check for a POST request
     if request.method == "POST" and form.validate_on_submit():
-        username = form.username
-        password = form.password
+        username = form.username.data
+        password = form.password.data
         
         # Query for the user with that username
-        user =  db.session.query(User).filter(User.username == username)[0]
+        user =  db.session.query(User).filter(User.username == username).first()
         # Check the password
         if user and user.password == password:
             # TODO: Keep track of the logged in user in the current session
             # TODO: Add logout option
             return render_template("index.html", username=username)
 
-        return render_template("login.html", message="Invalid credentials. Please try again.")
+        message = "Invalid credentials. Please try again."
         
 
-    return render_template("login.html", form=form)
+    return render_template("login.html", form=form, message=message)
 
 
 # ---------------------------------------------------
