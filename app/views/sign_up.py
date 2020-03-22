aae19d2a197b8df7a4f1ac365f0d1232ff76f0ee
lolo23jhon <lolo23jhon@gmail.com>
2020-03-21 21:45:57 -0700

    The sign up form registration page now shows correclty.

--- app/views/sign_up.py
+++ app/views/sign_up.py
@@ -11,41 +11,38 @@ from .sign_up_confirmation import *
 @app.route("/sign-up", methods=["GET","POST"])
 def sign_up():
 
-    form = SignUpForm()
+    form = SignUpForm()   
     
-    validation_error = False
-    new_user = None
-
     # TODO: Add validation for matching password with repeated password
-    if request.method == "POST":
+    if request.method == "POST" and form.validate_on_submit():
+        validation_error = False
 
-        if form.validate_on_submit():
+        # Check if email is available
+        if db.session.query(User).filter(User.email == form.email).count() != 0:
+            form.email.errors.append("That email address is already registered.")
+            validation_error = True
 
-            # Check if email is available
-            if db.session.query(User).filter(User.email == form.email).count() != 0:
-                form.email.errors.append("That email address is already registered.")
-                validation_error = True
-
-            # Check if username is available
-            if db.session.query(User).filter(User.username == form.username).count() != 0:
-                form.username.errors.append("That username is already taken.")
-                validation_error = True
-        
-        # Database validation failed, return the form with respective errors
-        if validation_error:
-            return render_template("sign_up.html", form=form)
-            
-        new_user = User(
-            form.first_name.data,
-            form.last_name.data,
-            form.email.data,
-            form.username.data,
-            form.password.data, 
-            datetime.now().ctime())  
-
-        # TODO: Add email confirmation to user registration
-        # No error, add new user to database
-        db.session.add(new_user)
-        db.session.commit()
+        # Check if username is available
+        if db.session.query(User).filter(User.username == form.username).count() != 0:
+            form.username.errors.append("That username is already taken.")
+            validation_error = True
     
-    return sign_up_confirmation()
+        # Database validation failed, return the form with respective errors
+        if not validation_error:
+            # Createa a new user object
+            new_user = User(
+                form.first_name.data,
+                form.last_name.data,
+                form.email.data,
+                form.username.data,
+                form.password.data, 
+                datetime.now().ctime())  
+
+            # TODO: Add email confirmation to user registration
+            # Add the new user to the database
+            db.session.add(new_user)
+            db.session.commit()
+
+            return sign_up_confirmation()
+
+    return render_template("sign_up.html", form=form)
