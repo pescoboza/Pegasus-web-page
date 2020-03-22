1d08ef37e230c473c5447760d914af9454c7e15e
lolo23jhon <lolo23jhon@gmail.com>
2020-03-21 21:16:55 -0700

    Fixed importation error in app/views/__init__.py.

--- app/views/sign_up_confirmation.py
+++ app/views/sign_up_confirmation.py
@@ -1,7 +1,7 @@
- from flask import render_template
- from .. import app
+from flask import render_template
+from .. import app
 
- @app.route("/signup-confirmation")
- def signup_confirmation(): 
+@app.route("/sign-up-confirmation")
+def signup_confirmation(): 
     message = "Thank you for registering.\n To activate your account, check your email and click on the confirmation link.".format(new_user.username, cover_email(new_user.email))
     return render_template("message.html", message=message)
