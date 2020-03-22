b6677914b4c8b6f063d9ab449b3f3e8eb2bec4f1
lolo23jhon <lolo23jhon@gmail.com>
2020-03-21 18:34:23 -0700

    Added database validation for sign up form in app/views/sign_up.py.

--- app/views/sign_up.py
+++ app/views/sign_up.py
@@ -11,6 +11,8 @@ from ..forms.sign_up_form import SignUpForm
 def sign_up():
 
     form = SignUpForm()
+    validation_error = False
+    new_user = None
 
     # TODO: Add validation for matching password with repeated password
     if request.method == "POST" and form.validate_on_submit():
@@ -22,5 +24,23 @@ def sign_up():
             form.password, 
             datetime.now().ctime())
 
-        email_available = bool(db.session.query(User).filter(User.email == new_user.email).count() == 0)
-        username_available = bool(db.session.query(User).filter(User.username == new_user).count() == 0)
+
+        # Check if email is available
+        if db.session.query(User).filter(User.email == new_user.email).count() != 0:
+            form.email.errors.append("That email address is already registered.")
+            validation_error = True
+
+        # Check if username is available
+        if db.session.query(User).filter(User.username == new_user).count() != 0:
+            form.username.errors.append("That username is already taken.")
+            validation_error = True
+
+    if validation_error:
+        return render_template("sign_up.html", form=form)
+    
+    # TODO: Add email confirmation to user registration
+    # No error, add new user to database
+    db.session.add(new_user)
+    db.session.commit()
+
+    return render_template("index.html", username=new_user)
