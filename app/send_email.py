8e276c586d295ad243370e2fb9fdc464cbc89189
lolo23jhon <lolo23jhon@gmail.com>
2020-03-27 21:54:46 -0700

    Added send_async_email() in send_email.py.

--- app/send_email.py
+++ app/send_email.py
@@ -2,6 +2,24 @@ from threading import Thread
 from flask import render_template
 from flask_mail import Mail, Message
 from . import app, mail
+from .decorators import async
+
+@async 
+def send_async_email(subject,recipients,text_body,html_body=None,cc=None,bcc=None):
+    try:
+        msg = Message(
+            subject=subject,
+            sender=app.config["MAIL_USERNAME"],
+            recipients=recipients,
+            cc=cc,
+            bcc=bcc,
+            body=text_body,
+            html=html_body 
+            )
+        return "200"    
+    except Exception, e:
+        return str(e)
+
 
 
 # TODO: set MAX_EMAIL flag in config.py.
