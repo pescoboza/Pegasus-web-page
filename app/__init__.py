771f186639fd8aa27febc7f55f7d6e711f2464a8
lolo23jhon <lolo23jhon@gmail.com>
2020-03-29 16:09:41 -0700

    Recreated config.py.

--- app/__init__.py
+++ app/__init__.py
@@ -11,24 +11,7 @@ from get_local_host import get_local_host
 # Configuration setup
 # ---------------------------------------------------
 app = Flask(__name__)
-# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ #
-#   TODO: SOLVE EMAIL MANIA ONCE AND FOR ALL!!!   #
-# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ #
-app.config["DEBUG"] = True
-app.config["TESTING"] = False
-app.config["SECRET_KEY"] = "pegasus-dev-team"
-app.config["SECURITY_PASSWORD_SALT"] = "Den Minecraft ist kaputt!"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SQLALCHEMY_DATABASE_URI"] = get_local_host()
-app.config["MAIL_SERVER"] = "smtp.gmail.com"
-app.config["MAIL_PORT"] = 465
-app.config["MAIL_USE_TLS"] = False
-app.config["MAIL_USE_SSL"] = True
-app.config["MAIL_SUPPRESS_SEND"] = False
-app.config["MAIL_ASCII_ATTACHMENTS"] = True
-app.config["MAIL_USERNAME"] = "pegasus.csn@gmail.com"
-app.config["MAIL_DEFAULT_SENDER"] = "pegasus.csn@gmail.com"
-app.config["MAIL_PASSWORD"] = "std::cout<<\"HelloWorld!\"<<std::endl;"
+app.config.from_object("config.Config")
 
 mail = Mail(app)
 
---
+++ config.py
@@ -0,0 +1,18 @@
+from get_local_host import get_local_host
+
+class Config:
+    DEBUG = True
+    TESTING = False
+    SECRET_KEY = "pegasus-dev-team"
+    SECURITY_PASSWORD_SALT = "Den Minecraft ist kaputt!"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = get_local_host()
+    MAIL_SERVER = "smtp.gmail.com"
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_SUPPRESS_SEND = False
+    MAIL_ASCII_ATTACHMENTS = True
+    MAIL_USERNAME = "pegasus.csn@gmail.com"
+    MAIL_DEFAULT_SENDER = "pegasus.csn@gmail.com"
+    MAIL_PASSWORD = "std::cout<<\"HelloWorld!\"<<std::endl;"
