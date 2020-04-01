ba4db82cfa0cfd765bd54218f723f7ae5bf173c8
lolo23jhon <lolo23jhon@gmail.com>
2020-03-31 21:25:38 -0700

    Added dynamic view for profile.

--- app/views/profile.py
+++ app/views/profile.py
@@ -1,7 +1,13 @@
+from flask import redirect, url_for, render_template
+from .. import app, db
+from ..models.user import User
 from decorators import check_confirmed
 
-
-@app.route("/profile", methods=["GET", "POST"])
+@app.route("/profile/<username>", methods=["GET", "POST"])
 @login_required
 @check_confirmed
-def profile():
+def profile(username):
+    user = db.session.query(User).filter(User.username == username).firs()
+    if user == None:
+        redirect(url_for("not_found"))
+    render_template("profile.html",user=user)
