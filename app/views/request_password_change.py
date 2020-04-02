5367b5dd5520060dba60816309bd16408fc6dea8
lolo23jhon <lolo23jhon@gmail.com>
2020-04-01 21:19:34 -0700

    Fixed view request_passwrod_change view not sending email.

--- app/views/request_password_change.py
+++ app/views/request_password_change.py
@@ -5,6 +5,8 @@ from ..forms.request_password_change_form import RequestPasswordChangeForm
 from ..token import generate_confirmation_token
 from ..send_email import send_email
 
+from sys import stderr
+
 @app.route("/forgot-password", methods=["GET","POST"])
 def request_password_change():
     form = RequestPasswordChangeForm()
@@ -15,12 +17,13 @@ def request_password_change():
 
         if user != None:
             token = generate_confirmation_token(user.email)
-            confirmation_url = url_for( "reset-password", url=token, _external=True)
+            confirmation_url = url_for("request_password_change", token=token, _external=True)
             
             text_body = "We received a request from you to reset your password. Please follow the link to do so:\n" + confirmation_url
             html_body = render_template("password_reset_confirmation_email.html", confirmation_url=confirmation_url)
 
             send_email("Password change request.", [user.email], text_body=text_body, html_body=html_body)
+
         flash("A password rest token has been sent to your email.")
     
     return render_template("password_reset.html", form=form)
