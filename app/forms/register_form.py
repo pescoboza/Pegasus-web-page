493f50af85cafc43b8e1eb9be66fceda872bc161
lolo23jhon <lolo23jhon@gmail.com>
2020-04-05 22:45:00 -0700

    Fixed errors in custom validators of register_form.py.

--- app/forms/register_form.py
+++ app/forms/register_form.py
@@ -29,11 +29,11 @@ class RegisterForm(FlaskForm):
     submit = SubmitField("Register")
 
     def validate_username(self, username):
-        user = db.session.query(User).filter(User.username == username).first()
+        user = db.session.query(User).filter(User.username == username.data).first()
         if user != None:
             raise ValidationError("Please choose a different username.")
 
     def validate_email(self, email):
-        user = db.session.query(User).filter(User.email == email).first()
+        user = db.session.query(User).filter(User.email == email.data).first()
         if user != None:
             raise ValidationError("Please use a different email address.")
