9915972185e43ffb9a7f371df3c0e315e76c93e2
lolo23jhon <lolo23jhon@gmail.com>
2020-04-07 19:43:34 -0700

    Change all datetimes to UTC.

---
+++ app/models/post.py
@@ -0,0 +1,8 @@
+from datetime import datetime 
+from .. import db
+
+class Post(db.Model):
+    __tablename__="posts"
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow())
