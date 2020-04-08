eef9a625330a14de0e044d8bbdd317567ad2bd09
lolo23jhon <lolo23jhon@gmail.com>
2020-04-08 14:56:32 -0700

    Updated decorators for user permissions.

--- app/decorators.py
+++ app/decorators.py
@@ -3,6 +3,7 @@ from flask import flash, redirect, url_for, session
 from flask_login import current_user
 from . import db
 from .models.user import User
+from .models.roles import Permission
 
 def check_is_authenticated(func):
     @wraps(func)
@@ -14,13 +15,19 @@ def check_is_authenticated(func):
         return func(*args, **kwargs)
     return decorated_function
 
+def permission_required(permission):
+    def decortor(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if not current_user.can(permission):
+                abort(403)
+            return f(*args, **kwargs)
+        return decorated_function
+    return decorator
+
 # If the user is admin it proceeds, else it sends you to the homepage silently
 def admin_required(func):
-    @wraps(func)
-    def decorated_function(*args, **kwargs):
-        user = User.query.filter_by(username=current_user.username).first()
-        if user != None and user.is_administrator == True:
-            return func(*args, **kwargs)
-        else:
-            return redirect(url_for("index"))
-    return decorated_function
+    return permission_required(Permission.ADMIN)(func)
+
+def moderator_required(func)
+    return permission_required(Permission.MODERATE)(func)
