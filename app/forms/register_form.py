dcda5a0caa4c6df0080435bda7b309cbd59b7f44
lolo23jhon <lolo23jhon@gmail.com>
2020-04-08 19:48:18 -0700

    Fixed typos and syntax errors in register_form.py.

--- app/forms/register_form.py
+++ app/forms/register_form.py
@@ -1,23 +1,23 @@
 from flask_wtf import FlaskForm
-from wtforms import TextField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import ValidationError,Required, Length, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Length, Email, EqualTo
 from .. import db
 from ..validators import create_new_field
 from ..models.user import User
 
 class RegisterForm(FlaskForm):
-    first_name = create_new_field["first_name"]
-    last_name = create_new_field["last_name"]
-    username = create_new_field["username"]
-    email = create_new_field["email"]
-    password = create_new_field["password"]
+    first_name = create_new_field("first_name")
+    last_name = create_new_field("last_name")
+    username = create_new_field("username")
+    email = create_new_field("email")
+    password = create_new_field("password")
     repeat_password = PasswordField(label="Repeat password", validators=[
-                                   Required(),  
+                                   DataRequired(),  
                                    EqualTo("password", 
                                    message="Passwords must match.")])
 
-    newsletter = Booleanfieldield()
-    accept_terms_and_conditions = Booleanfieldield(validators=[Required()])
+    newsletter = BooleanField()
+    accept_terms_and_conditions = BooleanField(validators=[DataRequired()])
 
     submit = SubmitField("Register")
 
