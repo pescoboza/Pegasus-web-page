c3714a93a333dce22bc1fa4ae4b28403283b4da7
lolo23jhon <lolo23jhon@gmail.com>
2020-04-11 17:01:36 -0700

    Fixed import errors.

--- app/models/follow.py
+++ app/models/follow.py
@@ -1,11 +1,9 @@
 from datetime import datetime
 from .. import app, db
-from .user import User
-
 
 class Follow(db.Model):
     __tablename__ = "follows"
     follower_id = db.Column(db.Integer, db.ForeignKey("users.id"), primary_key=True)
     followed_id = db.Column(db.Integer, db.ForeignKey("users.id"), primary_key=True)
-    timestamp = db.Column(db.DateTime, default=datetine.utcnow())
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow())
 
