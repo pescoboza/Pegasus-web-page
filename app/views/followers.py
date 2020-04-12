41337e8a757888a7bc26bf11d4ef382e7f0a4320
lolo23jhon <lolo23jhon@gmail.com>
2020-04-11 17:25:09 -0700

    Added views follow and followers.

---
+++ app/views/followers.py
@@ -0,0 +1,17 @@
+from flask import render_template, redirect, url_for, flash, request, current_app
+from .. import app
+from ..models.user import User
+
+
+@app.route("/followers/<username>")
+def followers(username):
+    user = User.query.filter_by(username=username).first()
+    if user == None:
+        flash("Invalid user.")
+        return redirect(url_for("index"))
+
+    page = request.args.get("page", 1, type=int)
+    pagination = user.followers.paginate(page, per_page=current_app.config["APP_FOLLOWERS_PER_PAGE"], error_out=False)
+    follows = [{"user": item.follower, "timestamp": item.timestamp} for item in pagination.items]
+
+    return render_template("followers.html", user=user, title="Followers of", endpoint="followers", pagination=pagination, follows=follows)
