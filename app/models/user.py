a3c00a754ba44e49d6a6e24b106e71e945355ba1
lolo23jhon <lolo23jhon@gmail.com>
2020-04-11 19:25:38 -0700

    Updated blog.py to allow the showing of followed posts.

--- app/models/user.py
+++ app/models/user.py
@@ -112,6 +112,9 @@ class User(UserMixin, db.Model):
 
     followers = db.relationship("Follow", foreign_keys=[Follow.followed_id], backref=db.backref("followed", lazy="joined"),
                                 lazy="dynamic", cascade="all, delete-orphan")
+    @property
+    def followed_posts(self):
+        return Post.query.join(Follow, Follow.followed_id == Post.author_id).filter(Follow.follower_id == self.id)
 
     def follow(self, user):
         if not self.is_following(user):
