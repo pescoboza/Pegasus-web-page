a77ea78a4a5b00389608fb979a3fc7e5634185c7
lolo23jhon <lolo23jhon@gmail.com>
2020-04-12 18:42:23 -0700

    Custom error handlers now work as expected.

--- app/views/error_handle.py
+++ app/views/error_handle.py
@@ -2,13 +2,13 @@ from flask import render_template
 from .. import app
 
 @app.errorhandler(404)
-def error_404():
-    render_template("404.html")
+def error_404(*args, **kwargs):
+    return render_template("404.html")
 
 @app.errorhandler(403)
-def error_403():
-    render_template("403.html")
+def error_403(*args, **kwargs):
+    return render_template("403.html")
 
 @app.errorhandler(500)
-def error_500():
-    render_template("500.html")
+def error_500(*args, **kwargs):
+    return render_template("500.html")
